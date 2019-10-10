79cb6a23f446488fd07262baec0eefd5ebe8a65d
march-dave <ysangy@gmail.com>
2019-10-09 20:04:45 -0400

    up to date

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
     plugins : [
         new HtmlWebpackPlugin({
-            title : '직원 정보 조회', 
+            title : '직원 정보 조회 2', 
             template : __dirname + '/assets/index.html', 
             filename : 'index.html'      
        }),
