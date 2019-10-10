0d92fcde705e77f26c3e905d2f8ade5459c40a2e
march-dave <ysangy@gmail.com>
2019-10-09 20:28:09 -0400

    webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,11 @@ module.exports = {
     devtool: 'source-map',
     mode: 'development',
     entry: {
-        main: __dirname + '/src/main.js'
-    }, 
-    // output: { 
-    //     path: __dirname + '/public', 
-    //     filename: '[name]-[hash].js',
-    //     publicPath: '/',
-    // },
+        main: __dirname + '/src/main.js'}, 
+    output: { 
+        path: __dirname + '/public', 
+        filename: '[name]-[hash].js' 
+    },
 
     module: {
         rules: [
@@ -41,19 +39,18 @@ module.exports = {
 
     plugins : [
         new HtmlWebpackPlugin({
-            // title : '직원 정보 조회2', 
-            // template : __dirname + '/assets/index.html', 
-            template : __dirname + '/src/public/index.html', 
-            // filename : 'index.html'      
+            title : '직원 정보 조회2', 
+            template : __dirname + '/assets/index.html', 
+            filename : 'index.html'      
        }),
     //    new UglifyJsPlugin()
     ],
 
     devServer: {
-        // contentBase: "./111111public",
-        // inline: true,
+        contentBase: "./public",
+        inline: true,
         port: 3000,
-        // historyApiFallback: true
+        historyApiFallback: true
 
     }
  };
---
+++ webpack.test.js
@@ -0,0 +1,59 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = { 
+    devtool: 'source-map',
+    mode: 'development',
+    entry: {
+        main: __dirname + '/src/main.js'
+    }, 
+    // output: { 
+    //     path: __dirname + '/public', 
+    //     filename: '[name]-[hash].js',
+    //     publicPath: '/',
+    // },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: ['@babel/plugin-proposal-object-rest-spread']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  { loader: "style-loader" },
+                  { loader: "css-loader",
+                    options: {modules: true}
+                    
+                    }
+                ]
+            }
+        
+        ]
+    },
+
+    plugins : [
+        new HtmlWebpackPlugin({
+            // title : '직원 정보 조회2', 
+            // template : __dirname + '/assets/index.html', 
+            template : __dirname + '/src/public/index.html', 
+            // filename : 'index.html'      
+       }),
+    //    new UglifyJsPlugin()
+    ],
+
+    devServer: {
+        // contentBase: "./111111public",
+        // inline: true,
+        port: 3000,
+        // historyApiFallback: true
+
+    }
+ };
